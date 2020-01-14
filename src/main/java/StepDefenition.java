103d4613658b8bbfb975c54ada4d548077c783af
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2020-01-14 17:16:39 +0200

    initial commit with initial working code

---
+++ src/main/java/StepDefenition.java
@@ -0,0 +1,13 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java8.En;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class StepDefenition implements En {
+
+    @Given("the search page is opened")
+    public void the_search_page_is_open(){
+
+        open("https://duckduckgo.com/");
+    }
+}
---
+++ src/test/resources/duckducksearch.feature
@@ -0,0 +1,6 @@
+Feature: DuckDuck Search
+
+Scenario: Search text on DuckDuck
+    Given the search page is opened
+    #When  text '' entered
+    #Then
